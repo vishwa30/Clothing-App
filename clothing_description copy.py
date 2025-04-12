a630f16fb404f65e75175e0dc1520920b23b630e
Vishwa Bhushan Dewangan <vishwabhushan30@gmail.com>
2025-04-12 13:19:07 +0530

    Changing low to high, changing prompt to be cautious about sweater

--- clothing_description copy.py
+++ clothing_description copy.py
@@ -20,7 +20,7 @@ class ClothingDescription:
                             "type": "image_url",
                             "image_url": {
                                 "url": f"data:image/jpeg;base64,{base64_image}",
-                                "detail": "high",
+                                "detail": "low",
                             },
                         },
                     ],
